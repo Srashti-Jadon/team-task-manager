7067e9bea32215907d82550a3020e594feeca5de
Srashti-Jadon <15prince2002@gmail.com>
2026-05-06 15:18:54 +0530

    fix database config for railway

--- config.py
+++ config.py
@@ -5,11 +5,9 @@ class Config:
 
     DATABASE_URL = os.environ.get('DATABASE_URL')
 
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
     if DATABASE_URL:
         SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace("postgres://", "postgresql://")
     else:
-        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "database.db")
+        SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
