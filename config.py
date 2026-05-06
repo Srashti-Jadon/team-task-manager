c25342f3a7a8c9f5ce314b3c4e9fc9546e4044aa
Srashti-Jadon <15prince2002@gmail.com>
2026-05-06 16:57:22 +0530

    fix railway postgres driver

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
